fea2f759dd344720f2786188202966a1ff931519
Thien Nguyen <thien.ngu25@gmail.com>
2024-04-10 10:09:12 +0200

    modify className Container-Global

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <main className="container">
+    <main className="container-Global">
       <Header />
     </main>
   );
