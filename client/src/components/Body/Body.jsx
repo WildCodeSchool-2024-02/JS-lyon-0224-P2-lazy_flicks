d331dd7ed6617ad0d56086cf3745db2280615e27
romain_delol <delol.romain@gmail.com>
2024-04-10 11:41:26 +0200

    modify body

---
+++ client/src/components/Body/Body.jsx
@@ -0,0 +1,31 @@
+import "./Body.css";
+
+function Body() {
+  return (
+    <main>
+      <div className="card">
+        <div className="genre">
+          <h2>Genre :</h2>
+          <p className="thriller">Thriller</p>
+        </div>
+        <div className="type">
+          <h2>Type :</h2>
+          <p className="series-movies">
+            <span className="series">Series</span>
+            <span>Movies</span>
+          </p>
+        </div>
+        <div className="time">
+          <h2>Time :</h2>
+          <p className="time-movies">120min</p>
+        </div>
+        <div className="search">
+          <button className="button-search" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+export default Body;
