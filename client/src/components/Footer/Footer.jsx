793d9b2cf656108dd73d310f5a970e6416b9e334
Thien Nguyen <thien.ngu25@gmail.com>
2024-04-10 10:06:37 +0200

    first commit for Footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="FooterLazy">
+      <p>Copyright LazyFlicks Wild Code School</p>
+    </footer>
+  );
+}
+
+export default Footer;
