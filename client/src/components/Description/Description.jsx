f8fe50fae9e7a2df15ff1f3edd1e94cbb6d16eff
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-04-10 11:42:25 +0200

    modify description.css and description.jsx

--- client/src/components/Description/Description.jsx
+++ client/src/components/Description/Description.jsx
@@ -11,7 +11,7 @@ function Description() {
       <div className="text">
         <h1>Gladiator</h1>
         <p>
-          <span>Release :</span> june 20 2000 | 2h35
+          <span>Release :</span> June 20 2000 | 2h35
         </p>
         <p>
           <span>By :</span> Ridley Scott
@@ -19,18 +19,19 @@ function Description() {
         <p>
           <span>Rate ⭐ :</span> 4.5/10
         </p>
-        <div>
-          <p>
-            The Roman general Maximus is the most faithful support of the
-            Emperor Marcus Aurelius, whom he led from victory to victory with
-            exemplary bravery and dedication. Jealous of Maximus' prestige, and
-            even more so of the emperor's love for him, Marcus Aurelius' son,
-            Commodus, brutally assumed power, then ordered the general's arrest
-            and execution. Maximus escapes his assassins but cannot prevent the
-            massacre of his family. Captured by a slave trader, he becomes a
-            gladiator and plots his revenge.
-          </p>
-        </div>
+        <p>
+          The Roman general Maximus is the most faithful support of the Emperor
+          Marcus Aurelius, whom he led from victory to victory with exemplary
+          bravery and dedication. Jealous of Maximus' prestige, and even more so
+          of the emperor's love for him, Marcus Aurelius' son, Commodus,
+          brutally assumed power, then ordered the general's arrest and
+          execution. Maximus escapes his assassins but cannot prevent the
+          massacre of his family. Captured by a slave trader, he becomes a
+          gladiator and plots his revenge.
+        </p>
+        <button className="button-watch" type="button">
+          Watch
+        </button>
       </div>
     </main>
   );
