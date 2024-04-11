2b042c22f1a5d1b50df92ac0cab1c2479d9dc3b8
romain_delol <delol.romain@gmail.com>
2024-04-11 14:03:16 +0200

    modif body

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,22 +1,6 @@
-import { useEffect, useState } from "react";
 import "./Body.css";
-import axios from "axios";
 
 function Body() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/movie/157336?api_key=d153b88aa2e55557f40150e3cf446757"
-      )
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console(error);
-      });
-  }, []);
   return (
     <main>
       <div className="card">
@@ -26,25 +10,22 @@ function Body() {
         </div>
         <div className="type">
           <h2>Type :</h2>
-          <p className="series-movies">
-            <span className="series">Series</span>
-            <span>Movies</span>
-          </p>
+          <div className="series">
+            <input type="checkbox" id="series" name="series" />
+            <label htmlFor="series">Series</label>
+          </div>
+          <div className="movies">
+            <input type="checkbox" id="movies" name="movies" />
+            <label htmlFor="movies">Movies</label>
+          </div>
         </div>
         <div className="time">
           <h2>Time :</h2>
           <p className="time-movies">120min</p>
         </div>
-        <div className="search">
-          <button className="button-search" type="button">
-            Search
-          </button>
-        </div>
-      </div>
-      <div>
-        {data && data.title && <p>{data.title}</p>}
-        {data && data.overview && <p>{data.overview}</p>}
-        {data && data.release_date && <p>{data.release_date}</p>}
+        <button className="button-search" type="button">
+          Search
+        </button>
       </div>
     </main>
   );
