4d0ceb7ae453f08b9221dc96efafabe65b38b40b
romain_delol <delol.romain@gmail.com>
2024-04-17 15:21:13 +0200

    button genre works

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -4,6 +4,11 @@ import Description from "../Description/Description";
 
 function Body() {
   const [isVisible, setIsVisible] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
+  const handleSearch = () => {
+    setIsVisible(true);
+  };
 
   return (
     <main>
@@ -11,58 +16,42 @@ function Body() {
         <div className="genre">
           <h2>Genre :</h2>
           <span className="GenreInput">
-            <select name="movie" id="movie-select">
-              <option value="Action">Action</option>
-              <option value="Aventure">Aventure</option>
-              <option value="Animation">Animation</option>
-              <option value="Comedy">Comedy</option>
-              <option value="Crime">Crime</option>
-              <option value="Documentary">Documentary</option>
-              <option value="Drama">Drama</option>
-              <option value="Family">Family</option>
-              <option value="Fantasy">Fantasy</option>
-              <option value="History">History</option>
-              <option value="Horror">Horror</option>
-              <option value="Music">Music</option>
-              <option value="Mystery">Mystery</option>
-              <option value="Romance">Romance</option>
-              <option value="Science Fiction">Science Fiction</option>
-              <option value="TV Movie">TV Movie</option>
-              <option value="Thriller">Thriller</option>
-              <option value="War">War</option>
-              <option value="Western">Western</option>
+            <select
+              name="movie"
+              id="movie-select"
+              onChange={(e) => setSelectedGenre(e.target.value)}
+            >
+              <option value="">Select a genre</option>
+              <option value="28">Action</option>
+              <option value="12">Adventure</option>
+              <option value="16">Animation</option>
+              <option value="35">Comedy</option>
+              <option value="80">Crime</option>
+              <option value="99">Documentary</option>
+              <option value="18">Drama</option>
+              <option value="10751">Family</option>
+              <option value="14">Fantasy</option>
+              <option value="36">History</option>
+              <option value="27">Horror</option>
+              <option value="10402">Music</option>
+              <option value="9648">Mystery</option>
+              <option value="10749">Romance</option>
+              <option value="878">Science Fiction</option>
+              <option value="10770">TV Movie</option>
+              <option value="53">Thriller</option>
+              <option value="10752">War</option>
+              <option value="37">Western</option>
             </select>
           </span>
         </div>
-        <div className="type">
-          <h2>Type :</h2>
-          <input type="checkbox" id="series" name="series" />
-          <label htmlFor="series">TV Shows</label>
-          <input type="checkbox" id="movies" name="movies" />
-          <label htmlFor="movies">Films</label>
-        </div>
-        <div className="time">
-          <h2>Time :</h2>
-          <span className="TimeInput">
-            <select name="pets" id="pet-select">
-              <option value="">Less than 2h</option>
-              <option value="">More than 2h</option>
-              <option value="">I don't have any friends</option>
-            </select>
-          </span>
-        </div>
-        <button
-          onClick={() => setIsVisible(!isVisible)}
-          className="button-search"
-          type="button"
-        >
+        <button onClick={handleSearch} className="button-search" type="button">
           {isVisible !== false ? "Search Again" : "Search"}
         </button>
       </div>
       <div>
-        {isVisible !== false && (
+        {isVisible && selectedGenre && (
           <div>
-            <Description />
+            <Description selectedGenre={selectedGenre} />
           </div>
         )}
       </div>
