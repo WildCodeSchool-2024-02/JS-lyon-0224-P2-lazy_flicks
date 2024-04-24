a4cc11afd279eaf4a0d09b2c9aabe3a968c55c87
Thien Nguyen <thien.ngu25@gmail.com>
2024-04-24 14:23:45 +0200

    fusion US21 and US16

--- client/src/components/Team/Team.jsx
+++ client/src/components/Team/Team.jsx
@@ -13,8 +13,8 @@ function Team() {
           src="src/assets/images/RomainImg.png"
           alt="Romain"
         />
-        <h2>Romain</h2>
-        <p>
+        <h2 className="TeamMember">ROMAIN</h2>
+        <p className="DescMember">
           Romain is the embodiment of warmth and joy in our workplace. His
           infectious laughter and positive energy have a way of lighting up the
           room, making even the toughest days feel a little brighter.
@@ -22,12 +22,11 @@ function Team() {
       </div>
       <div className="AlpDesc">
         <img className="AlpImg" src="src/assets/images/AlpImg.png" alt="Alp" />
-        <h2>Alp</h2>
-        <p>
+        <h2 className="TeamMember">ALP</h2>
+        <p className="DescMember">
           Alp is the epitome of kindness in our team. Always with a warm smile
           and a helping hand, he brightens up everyone's day. His genuine
-          empathy and willingness to support others make him an invaluable
-          member of our team.
+          empathy and willingness to support others.
         </p>
       </div>
       <div className="ThienDesc">
@@ -36,8 +35,8 @@ function Team() {
           src="src/assets/images/ThienImg.png"
           alt="Thien"
         />
-        <h2>Thien</h2>
-        <p>
+        <h2 className="TeamMember">THIEN</h2>
+        <p className="DescMember">
           Thien's creativity knows no bounds, consistently crafting designs that
           leave us in awe. His attention to detail and innovative approach bring
           a unique flair to every project he touches.
